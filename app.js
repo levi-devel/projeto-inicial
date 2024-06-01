df154f5c3187b1e8f6bc04bf634710797a5d1381
levi-devel <carloslevi12@gmail.com>
2024-05-31 20:37:07 -0400

    Jogo Numero Secreto

---
+++ app.js
@@ -0,0 +1,48 @@
+let numeroSecreto = gerarNumeroSecreto();
+let tentativas = 1;
+
+function editaTextonaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial(){
+    editaTextonaTela('h1', 'Jogo do número secreto');
+    editaTextonaTela('p', 'Escolha um número entre 1 e 10');
+}
+exibirMensagemInicial();
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    if(chute == numeroSecreto){
+        let palavraTentativas = tentativas > 1 ? 'tentativas': 'tentativa';
+        editaTextonaTela('h1', 'Acertou');
+        editaTextonaTela('p', 'Você descobriu o número secreto com '+tentativas+ ' '+palavraTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if(chute > numeroSecreto){
+            editaTextonaTela('p', 'O número secreto é menor');
+        }else{
+            editaTextonaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limpaCampo();
+    }
+}
+
+function gerarNumeroSecreto(){
+    return parseInt(Math.random() * 10 +1);
+}
+
+function limpaCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function botaoNovo(){
+    numeroSecreto = gerarNumeroSecreto();
+    exibirMensagemInicial();
+    limpaCampo();
+    tentativas = 1;
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
